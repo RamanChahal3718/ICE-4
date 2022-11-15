aca6ffc91e714b06abc07ba9900d49297fefcb0a
Raman <ramanchahal3718@gmail.com>
2022-11-15 15:53:28 -0500

    commit message

---
+++ Scripts/app.ts
@@ -0,0 +1,137 @@
+"use strict";
+//IIFE -- Immediately Invoked Function Expression
+// AKA - Self Executing Function 
+(function () {
+
+
+    /**
+     * This method saves our data to localStorage
+     *z
+     * @param {any[]} contactList
+     */
+    function SaveContactListData(contactList: any[]): void {
+
+        let count = 0;
+        for (const contact of contactList) {
+            let newContact = new Contact(contact.FullName, contact.ContactNumber, contact.EmailAddress);
+            localStorage.setItem(count.toString(), newContact.toJSON());
+            count++;
+        }
+
+    }
+
+    /**
+     * This method reads our data from localStorage and returns a Contact Array
+     *
+     * @returns {Contact[]}
+     */
+    function LoadContactListData(): Contact[] {
+        // create an empty Contact Array Container
+        let ContactArray = new Array<Contact>();
+
+        let keys = Object.keys(localStorage);
+        for (let key of keys) {
+            let newContact = new Contact();
+            newContact.fromJSON(localStorage.getItem(key));
+            ContactArray.push(newContact);
+
+            console.log(newContact)
+        }
+
+        return ContactArray;
+    }
+
+    /**
+     * This method loads the Header and the Page Content
+     *
+     */
+    function LoadHeader(): void {
+
+        console.log("Loading Header...");
+
+        $.get("./Views/components/header.html", function (html_data) {
+
+            $("header").html(html_data);
+
+            // Activate the Home Link on the initial load
+            $("li>a#Home").addClass("active");
+
+            $("li>a").on("click", function (event) {
+                event.preventDefault();
+
+                //change Title
+                document.title = $(this).prop("id") as string;
+
+                // Change URL
+                history.pushState({}, "", "/" + document.title);
+
+                //remove the active class from each list item
+                $("li>a").each(function () {
+                    $(this).removeClass("active");
+                });
+
+                // Activate the current Link
+                $("li>a#" + document.title).addClass("active");
+
+                LoadContent();
+            });
+
+
+        });
+    }
+
+
+    /**
+     *This method injects the Page Content
+     *
+     */
+    function LoadContent(): void {
+
+        console.log("Loading Content...");
+
+        let contentLink = document.title.toLowerCase();
+
+
+        $.get("./Views/content/" + contentLink + ".html", function (html_data) {
+            $("main").html(html_data);
+        });
+
+    }
+
+
+    /**
+     *This method loads and injects the footer content
+     *
+     */
+    function LoadFooter(): void {
+        console.log("Loading Footer...");
+
+        $.get("./Views/components/footer.html", function (html_data) {
+
+            $("footer").html(html_data);
+        });
+    }
+
+
+
+    // First method of using functions
+    function Start() {
+
+        console.log("App Started!");
+        // initial load
+        document.title = "Home";
+        // Change URL
+        history.pushState({}, "", "/Home");
+
+        LoadContent();
+
+        LoadHeader();
+
+        LoadFooter();
+
+
+    }
+
+    window.addEventListener("load", Start);
+})();
+
---
+++ Scripts/contact.js
@@ -0,0 +1,74 @@
+class Contact {
+    // private instance members (fields)
+    fullName;
+    contactNumber;
+    emailAddress;
+    // public properties
+    get FullName() {
+        return this.fullName;
+    }
+    set FullName(name) {
+        this.fullName = name;
+    }
+    get ContactNumber() {
+        return this.contactNumber;
+    }
+    set ContactNumber(contactNumber) {
+        this.contactNumber = contactNumber;
+    }
+    get EmailAddress() {
+        return this.emailAddress;
+    }
+    set EmailAddress(emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+    // constructor
+    /**
+     *Creates an instance of Contact.
+     * @param {string} [fullName="unknown name"]
+     * @param {string} [contactNumber="no contact number"]
+     * @param {string} [emailAddress="unknown email address"]
+     * @memberof Contact
+     */
+    constructor(fullName = "unknown name", contactNumber = "no contact number", emailAddress = "unknown email address") {
+        this.FullName = fullName;
+        this.ContactNumber = contactNumber;
+        this.EmailAddress = emailAddress;
+    }
+    // public methods
+    /**
+     * This method overrides the built-in toString method for the Contact class
+     *
+     * @returns {string}
+     * @memberof Contact
+     */
+    toString() {
+        let outputString = "";
+        outputString += `Full Name     : ${this.FullName}\n`;
+        outputString += `Contact Number: ${this.ContactNumber}\n`;
+        outputString += `Email Address : ${this.EmailAddress}\n`;
+        return outputString;
+    }
+    /**
+     * This method converts class Data Members to a comma-separated list compatible with JSON
+     *
+     * @returns {string}
+     * @memberof Contact
+     */
+    toJSON() {
+        return `${this.FullName},${this.ContactNumber},${this.EmailAddress}`;
+    }
+    /**
+     * This method reads data from a comma-separated list and assigns it to class Data Members
+     *
+     * @param {string} data
+     * @memberof Contact
+     */
+    fromJSON(data) {
+        let stringArray = data.split(",");
+        this.FullName = stringArray[0];
+        this.ContactNumber = stringArray[1];
+        this.EmailAddress = stringArray[2];
+    }
+}
+//# sourceMappingURL=contact.js.map
